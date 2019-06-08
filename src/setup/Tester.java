e41481ef86c31a2dbdc5c82c4a95a6157b7a3a1d
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-06-08 12:12:35 -0400

    testing download functionality

--- src/setup/Tester.java
+++ src/setup/Tester.java
@@ -14,19 +14,22 @@ public class Tester {
   public static void main(String[] args) throws IOException {
 //    System.out.println(getGradleVersion());
 
-    String[] command = new String[2];
-    command[0] = "powershell.exe";
-    command[1] = "gradle -version";
+//    String[] command = new String[2];
+//    command[0] = "powershell.exe";
+//    command[1] = "gradle -version";
 //    command[1] = "/c";
 //    command[1] = String
 //        .format("Get-Location; Set-Location -Path %s; Get-Location; .\\gradlew build; .\\gradlew shuffleboard", "C:\\Users\\mariu\\Downloads\\RobotTest");
-    Process process = Runtime.getRuntime().exec(command);
+//    Process process = Runtime.getRuntime().exec(command);
 
-    stdInput = new BufferedReader(new
-        InputStreamReader(process.getInputStream()));
+//    stdInput = new BufferedReader(new
+//        InputStreamReader(process.getInputStream()));
 
-    String collect = stdInput.lines().collect(Collectors.joining());
+//    String collect = stdInput.lines().collect(Collectors.joining());
 //    stdError.lines().forEach(System.out::println);
+
+
+    Controller.download("https://github.com/wpilibsuite/GradleRIO/tree/master/examples/java/", ".\\Hello");
 
   }
 }
